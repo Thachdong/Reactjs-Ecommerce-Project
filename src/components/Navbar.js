aaf2a3a5c010a2de2407338ce1605164866c4179
thachdong <you@example.com>
2019-08-23 02:33:08 +0700

    completed navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import { ButtonContainer } from './Button';
 import logo from '../logo.svg';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-primary mx-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
-                  <img src={logo} alt="store" className="navbar-brand" />
+                  <img src={logo} alt="store" className="navbar-brand" width="30px" />
                 </Link>
-                <ul className="navbar align-items-center">
+                <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
                             Products
                         </Link>
                     </li>
                 </ul>
-            </nav>
+                <Link to="/cart" className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />    
+                        </span>
+                        my cart
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
         );
     }
 }
 
 export default Navbar;
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize !important;
+    }
+`;
